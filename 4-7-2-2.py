feb85f39f9495a08727043bc67ef5a6dceb04256
hironori <hironori.uchibori.6p@stu.hosei.ac.jp>
2021-02-25 22:41:32 +0900

    指数・対数

---
+++ 4-7-2-2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-4, 4, 100)
+y = (x - 1)** 2 + 2
+logy = np.log(y)
+
+plt.figure(figsize=(4, 4))
+plt.plot(x, y, 'black', linewidth=3)
+plt.plot(x, logy, 'cornflowerblue', linewidth=3)
+plt.xticks(range(-4, 5, 1))
+plt.yticks(range(-4, 9, 1))
+plt.xlim(-4, 4)
+plt.ylim(-4, 8)
+plt.grid(True)
+plt.show()
